de83cd28dfa277cda4a35b36802735cb429ba0d0
Daniel Ashbrook <dan@danielashbrook.com>
2022-01-09 13:21:14 +0100

    mostly working, time to make a class

--- Untitled1.sync.py
+++ Untitled1.sync.py
@@ -56,11 +56,12 @@ g = gcode.GcodeFile('/Users/dan/r/thread_printer/stl/test1/main_body.gcode')
 thread.layers_to_geom(g)
 
 # %%
+# Convert fusion thread to Geometry3D
 #units from fusion are always in cm, so we need to convert to mm
 tpath = np.array(pickle.load(open('/Users/dan/r/thread_printer/stl/test1/thread_from_fusion.pickle', 'rb'))) * 10
 thread_transform = [131.164, 110.421, 0]
 tpath += [thread_transform, thread_transform]
-thread_geom = [Segment(Point(*s), Point(*e)) for s,e in tpath]
+thread_geom = tuple([Segment(Point(*s), Point(*e)) for s,e in tpath])
 
 # %%
 def xyz(*args, **kwargs):
@@ -105,23 +106,26 @@ def fig_del_by_names(fig, *names):
 
 
 def pz(p):
-	"""Set z to 0 and return."""
+	"""Return a new Point with z set to 0."""
+	p = deepcopy(p)
 	p.z = 0
 	return p
 
 # %%
-# Plot steps
 styles = {
-	'gc_segs':      {'mode': 'lines', 'line': dict(color='green', width=1)},
-	'th_traj':      {'mode': 'lines', 'line': dict(color='cyan', dash='dot', width=1)},
-	'thread_seg':   {'mode': 'lines', 'line': dict(color='red', dash='dot', width=1)},
-	'isec_gcode':   {'mode': 'lines', 'line': dict(color='yellow', width=1)},
-	'layer_thread': {
+	'gc_segs':         {'mode': 'lines', 'line': dict(color='green', width=1)},
+	'th_traj':         {'mode': 'lines+markers', 'line': dict(color='cyan', dash='dot', width=1)},
+	'thread_seg':      {'mode': 'lines+markers', 'line': dict(color='red', dash='dot', width=2)},
+	'isec_gcode':      {'mode': 'lines', 'line': dict(color='yellow', width=1)},
+	'anchored_thread': {'mode': 'lines+markers', 'line': dict(color='magenta', dash='dot', width=1)},
+	'anchor_point':    {'mode': 'markers', 'marker': dict(color='lime', symbol='x', size=8)},
+	'layer_thread':    {
 		'marker': dict(color='orange', size=4),
-		'line':   dict(color='red', dash='dot', width=3),
+		'line':   dict(color='red', dash='dot', width=1),
 	},
 }
 
+# %%
 def plot_steps(layer, thread_geom):
 	#Separate the layer lines that do (intersected_gc) and don't (gc_segs)
 	# intersect the thread in this layer
@@ -132,7 +136,6 @@ def plot_steps(layer, thread_geom):
 	gc_segs = list(gc_segs)
 
 	frames = []
-	titles = []
 
 	#List of traces to show in all frames
 	always_show = []
@@ -145,10 +148,11 @@ def plot_steps(layer, thread_geom):
 	# segment is the thread anchor point on the bed. Put the thread away from the
 	# model to begin with.
 	anchor = thread_geom[0].start_point
-	titles.append('Start')
 	frames.append(always_show + [
 		go.Scatter(**seg_xy(Segment(anchor, Point(anchor.x, layer.extents()[1][1], 0)),
-		name='th_traj', **styles['th_traj']))])
+			name='th_traj', **styles['th_traj'])),
+		go.Scatter(**xy(anchor, name='anchor_point', **styles['anchor_point'])),
+	])
 
 	#Now generate the frames of the animation:
 	# 1. Rotate the thread from its current anchor point to overlap the gcode segment that
@@ -164,6 +168,7 @@ def plot_steps(layer, thread_geom):
 		# segment
 		if not (enter or exit or gclines):
 			thread_seg.line.color = 'gray'
+			thread_seg.name = 'thread_seg_no_inter'
 			frames.append(always_show + [thread_seg])
 			continue
 
@@ -174,13 +179,13 @@ def plot_steps(layer, thread_geom):
 		# captured or points if entering/exiting)
 		if enter and exit:
 			point = enter
-			kwargs = xy(enter, exit, mode='lines', name='layer_thread')
+			kwargs = xy(enter, exit, mode='lines+markers', name='layer_thread')
 		elif enter or exit:
 			point = enter or exit
 			kwargs = xy(point, mode='markers', name='enter' if enter else 'exit')
 		else:
 			point = seg.start_point
-			kwargs = seg_xy(seg, mode='lines', name='seg.start_point')
+			kwargs = seg_xy(seg, mode='lines+markers', name='seg.start_point')
 
 		kwargs.update(styles['layer_thread'])
 		layer_thread = go.Scatter(**kwargs)
@@ -189,22 +194,32 @@ def plot_steps(layer, thread_geom):
 		# carrier
 		th_traj = go.Scatter(xy(anchor,
 			deepcopy(point).move(seg.line.dv.unit() * 100), #extend line
-			**styles['th_traj']))
+			name='th_traj', **styles['th_traj']))
 
 		#Set the anchor to the last place the thread gets captured
 		# TODO: did it actually get captured? If it exited, it didn't! Then we need to
 		# move the thread out of the way.
+		anchor_old = anchor
 		anchor = min(gcinter, key=lambda p:pz(seg.end_point).distance(pz(p)))
 
 		frames.append(always_show + [
+			thread_seg,
 			isec_gcode,
 			th_traj,
 			layer_thread,
 		])
-			
-		#Going forward, always show the placed gcode
-		isec_gcode_new = go.Scatter(**seg_xy(*gclines, **styles['gc_segs']))
-		always_show.append(isec_gcode_new)
+
+		#Going forward, always show:
+		# the placed gcode
+		isec_gcode_new = go.Scatter(**seg_xy(*gclines, name='isec_gcode', **styles['gc_segs']))
+		# the old anchor
+		old_anchor = go.Scatter(**xy(anchor_old, name='old_anchor', **styles['anchor_point']))
+		old_anchor.marker.color = 'gray'
+		# the thread up to the anchor
+		anchored_thread = go.Scatter(**seg_xy(Segment(anchor, anchor_old),
+			name='anchored_thread', **styles['anchored_thread']))
+
+		always_show.extend([isec_gcode_new, anchored_thread, old_anchor])
 
 	return frames
 
@@ -219,8 +234,10 @@ def show_each_frame(frames):
 				#height=750,
 				margin=dict(l=0, r=20, b=0, t=0, pad=0),
 				showlegend=False,)
+		print(', '.join([trace.name or 'XX' for trace in frame]))
 		fig.show('notebook')
 
 # %%
+page_wide()
 frames = plot_steps(g.layers[49], thread_geom)
 show_each_frame(frames)
